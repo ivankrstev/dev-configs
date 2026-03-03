bb2f6789f8549d9c4d65570e8edd28979701fb59
Ivan Krstev <ivankrstev246@gmail.com>
2026-03-03 21:04:11 +0100

    docs: add comments to setting history options

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -6,8 +6,8 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_FIND_NO_DUPS
-setopt SHARE_HISTORY
-setopt APPEND_HISTORY
-setopt INC_APPEND_HISTORY
+setopt HIST_IGNORE_ALL_DUPS # Remove older duplicates from history
+setopt HIST_FIND_NO_DUPS # Don't display duplicates in history search
+setopt SHARE_HISTORY # Share history across all sessions
+setopt APPEND_HISTORY # Append to history file instead of overwriting
+setopt INC_APPEND_HISTORY # Write to history file immediately, not when shell exits

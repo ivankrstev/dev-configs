700012382a6469af1e2b2e2adb048e0c38d818c6
Ivan Krstev <ivankrstev246@gmail.com>
2026-07-11 19:18:07 +0200

    feat: add custom paths for android studio

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -18,3 +18,12 @@ export LESS_TERMCAP_us=$'\e[1;4;31m'
 # Add custom paths if needed
 # export PATH="$HOME/bin:$PATH"
 path=($HOME/.npm-global/bin $path)
+PATH="$PATH:/home/ivank/.dotnet/tools"
+
+# Setup Android Studio environment variables(OPTIONAL - only if you have Android Studio installed)
+export PATH="$PATH:/usr/local/android-studio/bin"
+export ANDROID_HOME="$HOME/Android/Sdk"
+export ANDROID_SDK_ROOT="$ANDROID_HOME"
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
+export PATH="$PATH:$ANDROID_HOME/emulator"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
